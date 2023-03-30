66b17bba531ad91ebbeac3214688032fcc7a32a7
Fuat Ulugay <fuat.ulugay@trilogy.com>
2023-03-30 21:51:55 +0300

    test backend

---
+++ app1/backend.tf
@@ -0,0 +1,31 @@
+terraform {
+  
+  backend "azurerm" {
+    resource_group_name  = "FuatExperiments"
+    storage_account_name = "terraformtestff"
+    container_name       = "terraform"
+    key                  = "app1.tfstate"
+    subscription_id      = "b2020ded-fdab-492a-91e0-b4e04d9ef6df"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "testimg"
+
+    workspaces {
+      name = "learn-terraform-github-actions-azure"
+    }
+  }
+}
+
--- app1/main.tf
+++ app1/main.tf
@@ -1,27 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
 
-  cloud {
-    organization = "testimg"
-
-    workspaces {
-      name = "learn-terraform-github-actions-azure"
-    }
-  }
-}
 
 provider "azurerm" {
   features {}
