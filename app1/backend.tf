176d97680c77436fde139af2507e22ef42df9935
Fuat Ulugay <fuat.ulugay@trilogy.com>
2023-03-31 13:47:14 +0300

    test backend

--- app1/backend.tf
+++ app1/backend.tf
@@ -31,9 +31,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant
-  subscription_id = var.subscription_id
+  use_msi = true
 }
